d4ba199ed9829ba8ba9aee38718ff2705a0e1e82
Wanghao Xu <xu.wanghao@163.com>
2021-08-27 18:02:40 +0800

    Update main.m

--- main.m
+++ main.m
@@ -9,29 +9,30 @@ WIRE_WIDTH = 50;
 % first row describes the wire resistance info
 RESISTANCE_INFO = [0,0,0.1;100,200,0.6;];
 
-ELECTRONS_NUM = 100;
+ELECTRONS_NUM = 10;
 
 FIELD_FACTOR = 1;
 FIELD_DIRECTION = [-1,0];
 
 
-time_frame = 0;
+time_stamp = 0;
 
 electrons_pos = zeros(ELECTRONS_NUM, 2);
 % x coordinates, col subscripts, start from 1
-electrons_pos(:,1) = randi([1,WIRE_LENGTH], ELECTRONS_NUM, 1)
+electrons_pos(:,1) = randi([1,WIRE_LENGTH], ELECTRONS_NUM, 1);
 % y coordinates, row subscripts, start from 1
-electrons_pos(:,2) = randi([1,WIRE_WIDTH], ELECTRONS_NUM, 2)
+electrons_pos(:,2) = randi([1,WIRE_WIDTH], ELECTRONS_NUM, 1);
 
 electrons_vel = zeros(ELECTRONS_NUM, 2);
 
 
 resistance_map = RESISTANCE_INFO(1,3) * ones(WIRE_WIDTH, WIRE_LENGTH);
 for res_info = RESISTANCE_INFO(2:end,:)'
-	resistance_map(:,[res_info(1):res_info(2)]) = res_info(3)
+	resistance_map(:,[res_info(1):res_info(2)]) = res_info(3);
 end
 imagesc(resistance_map)
 title("resistance_map")
+pause(0.5)
 
 
 field_static = FIELD_FACTOR * FIELD_DIRECTION;
@@ -41,13 +42,18 @@ while true
 
 	% randomly set some elec velocities into 0 based on the resistance
 	elecs_pos_idx = sub2ind( ...
-		size(RESISTANCE_INFO),electrons_pos(:,2),electrons_pos(:,1));
-	res_elecs_on = RESISTANCE_INFO(elecs_pos_idx);
-	electrons_vel[rand(ELECTRONS_NUM,1)<=res_elecs_on,:] = [0,0]
+		size(resistance_map),electrons_pos(:,2),electrons_pos(:,1));
+	res_elecs_on = resistance_map(elecs_pos_idx);
+	electrons_vel(rand(ELECTRONS_NUM,1)<=res_elecs_on,:) = 0;
 
 	electrons_pos = electrons_pos + electrons_vel;
 
 	% the wire surfase is a doughnut
-	electrons_pos(:,1) = mod(electrons_pos(:,1)-1, WIRE_LENGTH) + 1
-	electrons_pos(:,2) = mod(electrons_pos(:,2)-1, WIRE_WIDTH) + 1
+	electrons_pos(:,1) = mod(electrons_pos(:,1)-1, WIRE_LENGTH) + 1;
+	electrons_pos(:,2) = mod(electrons_pos(:,2)-1, WIRE_WIDTH) + 1;
+
+	time_stamp = time_stamp + 1;
+	plot(electrons_pos(:,1), electrons_pos(:,2), 'o');
+	axis([0,WIRE_LENGTH,0,WIRE_WIDTH]);
+	pause(0.1)
 end
